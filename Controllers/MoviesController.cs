79e4577507c9517c8a11d13d6688e34b716255f8
Phil George <phil@philgeorge.com>
2020-12-29 11:22:47 +0100

    testing routing

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -11,6 +12,34 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
+            //return Content("Wawaaaa ");
+            //return HttpNotFound("Gone! Is no longer there...");
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, toto = "happy" });
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? id, string sortedBy)
+        {
+            if (!id.HasValue)
+                id = 1;
+
+            if (String.IsNullOrWhiteSpace(sortedBy))
+                sortedBy = "Name";
+
+            //return Content($"The id={id} and sortedBy={sortedBy}");
+            return Content( string.Format("The id={0} and sortedBy={1}", id, sortedBy));
+        }
+
+        [Route("movies/released/{year:regex(\\d{2})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
     }
 }
