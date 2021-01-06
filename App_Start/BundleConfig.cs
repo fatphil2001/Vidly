d0154b8279fbf1d55f2127c032b446a8888861be
Phil George <phil@philgeorge.com>
2021-01-06 11:30:19 +0100

    Add jqueryUI tu project

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -22,9 +22,17 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-{version}.js"));
+
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/cssjqueryui").Include(
+                      "~/Content/themes/base/jquery-ui.css"));
+
         }
     }
 }
