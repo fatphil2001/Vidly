f8274c5681d6b43266a3e0429ab859ee983b5267
Phil George <phil@philgeorge.com>
2021-01-12 11:33:37 +0100

    Add basic rental model and empty Web API

---
+++ Dtos/NewRentalDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Vidly.Dtos
+{
+    public class NewRentalDto
+    {
+        public int ClientId { get; set; }
+        public IEnumerable<int> MovieIds { get; set; }
+    }
+}
