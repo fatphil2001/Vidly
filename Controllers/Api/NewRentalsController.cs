32624c51fa97e1ea1fa673a04321dab1de81d8a7
Phil George <phil@philgeorge.com>
2021-01-12 11:33:37 +0100

    Add basic rental model and empty Web API

---
+++ Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        // Customer rents movies
+        // 
+        [HttpPost]
+        public IHttpActionResult RentMovies(NewRentalDto rentalDto)
+        {
+            return Ok();
+        }
+
+    }
+}
